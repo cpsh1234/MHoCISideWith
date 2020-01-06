b197d415cd8d744d341d5063d2588a30b0f178ae
cpsh1234 <58405996+cpsh1234@users.noreply.github.com>
2020-01-06 15:26:30 +0000

    Create catagories.js

---
+++ catagories.js
@@ -0,0 +1,41 @@
+catagories = [
+    {
+        "catagory": "Healthcare"
+        "questions": hquestions.js
+    },
+    {
+        "catagory": "Education"
+        "questions": edquestions.js
+    },
+    {
+        "catagory": "Social"
+        "questions": squestions.js
+    },
+    {
+        "catagory": "Constitution"
+        "questions": cquestions.js
+    },
+    {
+        "catagory": "Foreign Relations"
+        "questions": fquestions.js
+    },
+    {
+        "catagory": "Environment"
+        "questions": enquestions.js
+    },
+    {
+        "catagory": "Economy"
+        "questions": ecquestions.js
+    },
+    {
+        "catagory": "Immigration"
+        "questions": iquestions.js
+    },
+    {
+        "catagory": "Transport"
+        "questions": tquestions.js
+    },
+    {
+        "catagory": "Defence"
+        "questions": dquestions.js
+    },
