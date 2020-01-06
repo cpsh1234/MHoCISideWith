4c66fae4cc02b634eb6277adbf6f33f447e7b4b7
cpsh1234 <58405996+cpsh1234@users.noreply.github.com>
2020-01-06 18:48:52 +0000

    Update questions.js

--- questions.js
+++ questions.js
@@ -286,5 +286,602 @@ questions = [
             "lib": -10,
             "cit": 10
         }
+    },
+    
+    //Constitution 9 questions
+    {
+        "question": "16-year olds should be allowed to vote.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "The courts of the land need to be streamlined and simplified.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "The monarchy should be abolished.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "Only English MPs should be allowed to vote on laws that affect England only.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "The United Kingdom should adopt federalism.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "The House of Lords be abolished.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "Corporations and Unions should be allowed to donate money to political parties.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "Convicted Criminals should be afforded the right to vote.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "Convicted Criminals should be afforded the right to stand for election.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    
+    
+    
+    //Environment 13 questions
+    {
+        "question": "The UK should subsidise replacements of fossil fuels.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "The UK should immediately stop exporting its waste to other countries.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "The UK should be Carbon-Neutral by 2030.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "The UK should be Carbon-Neutral by 2050.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "All pesticides that might harm bees should be banned.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "There should be a tax on single use coffee cups.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "All new houses should have smart meters automatically installed.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "The UK should exploit geothermal power.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "All overhead electric cables should be buried.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "The UK should declare a climate emergency.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "It should be illegal to raise caged hens.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "The UK should support fracking.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "The UK government should invest more into nuclear power.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    
+    
+    
+    //Economy 17 Questions
+    {
+        "question": "The UK should nationalise Water.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "The UK should nationalise Royal Mail.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "The UK should provide free broadband for all.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "Tax should be replaced by a single flat tax on income.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "Business rates should be reduced.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "There should be a tax free allowance on corporation tax.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "The TV licence should be abolished.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "Inheritance tax should be abolished.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "The state should, no matter what, always aim to achieve a tax surplus.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "The rich are too highly taxed.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "All sin taxes should be abolished.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "There should be a tax on abandoned homes.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "VAT on energy bills should be scrapped.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "Income tax should be increased by 1% to fund the NHS.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "There should be a tax on frequent fliers.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "All public services, other than the police and army, should be franchised out to private companies.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "Tax brackets should be replaced by a smoother tax system.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    
+    
+    
+    //Transport 6 questions
+    {
+        "question": "The train lines should be nationalised.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "HS2 should be scrapped.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "UK airports should be able to build new runways.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "Train lines should be electrified.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "Roads should be privatised to reduce car tax.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "Rail fares should be frozen for the next five years.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    
+    
+    //Foreign Relations, Immigration and Defence 6, 3, 4 questions
+    {
+        "question": "The UK should recognise Palestine as a separate nation.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "The UK should leave the EU.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "The UK should commit at least 0.7% of GDP to foreign aid.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "The UK should not give foreign aid to any country that has a space program.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "The UK should not give foreign aid to any country that has a nuclear program.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "The UK should try to impose our values on the rest of the world.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "By 2022 the UK should accept no more than 1000 asylum seekers per year.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "All foreign nationals who aid the armed forces should receive british citizenship.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "The UK should have a points-based immigration system.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "Trident should be scrapped.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "The UK should stop selling weapons to Saudi Arabia.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "Military service in the Armed forces should be compulsory.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
+    },
+    {
+        "question": "The UK should be able to assassinate any perceived threat to national security without trial.",
+        "effect": {
+            "lab": 10,
+            "con": -5,
+            "ld": 10,
+            "lib": -10,
+            "cit": 10
+        }
     }
 ];
