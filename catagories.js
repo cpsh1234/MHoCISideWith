6d2af3c3fb3eed4c5c216b86eb22a7f6a3e4d6c0
cpsh1234 <58405996+cpsh1234@users.noreply.github.com>
2020-01-06 18:04:27 +0000

    Update catagories.js

--- catagories.js
+++ catagories.js
@@ -1,7 +1,98 @@
 catagories = [
     {
         "catagory": "Healthcare",
-        "questions": hquestions.js
+        "questions": = [
+                        {
+                            "question": "The NHS needs to have higher real-terms funding.",
+                            "effect": {
+                                "lab": 10,
+                                "con": -5,
+                                "ld": 10,
+                                "lib": -10,
+                                "cit": 10
+                            }
+                        },
+                        {
+                            "question": "All prescriptions should be free on the NHS, no matter your income.",
+                            "effect": {
+                                "lab": 10,
+                                "con": -5,
+                                "ld": 10,
+                                "lib": -10,
+                                "cit": 10
+                            }
+                        },
+                        {
+                            "question": "All dental work should be free, no matter your income.",
+                            "effect": {
+                                "lab": 10,
+                                "con": -5,
+                                "ld": 10,
+                                "lib": -10,
+                                "cit": 10
+                            }
+                        },
+                        {
+                            "question": "It is immoral for any private company to profit from the NHS.",
+                            "effect": {
+                                "lab": 10,
+                                "con": -5,
+                                "ld": 10,
+                                "lib": -10,
+                                "cit": 10
+                            }
+                        },
+                        {
+                            "question": "It is unfair for people who pay for private health insurance to also pay for the NHS.",
+                            "effect": {
+                                "lab": 10,
+                                "con": -5,
+                                "ld": 10,
+                                "lib": -10,
+                                "cit": 10
+                            }
+                        },
+                        {
+                            "question": "No one but UK nationals should get free treatment from the NHS, no matter their condition.",
+                            "effect": {
+                                "lab": 10,
+                                "con": -5,
+                                "ld": 10,
+                                "lib": -10,
+                                "cit": 10
+                            }
+                        },
+                        {
+                            "question": "The NHS should be abolished.",
+                            "effect": {
+                                "lab": 10,
+                                "con": -5,
+                                "ld": 10,
+                                "lib": -10,
+                                "cit": 10
+                            }
+                        },
+                        {
+                            "question": "Doctors deserve to be paid more.",
+                            "effect": {
+                                "lab": 10,
+                                "con": -5,
+                                "ld": 10,
+                                "lib": -10,
+                                "cit": 10
+                            }
+                        },
+                        {
+                            "question": "It is right that as prescription drug prices increase that burden should be passed on to those who use the drugs.",
+                            "effect": {
+                                "lab": 10,
+                                "con": -5,
+                                "ld": 10,
+                                "lib": -10,
+                                "cit": 10
+                            }
+                        }
+                    ];
     },
     {
         "catagory": "Education",
